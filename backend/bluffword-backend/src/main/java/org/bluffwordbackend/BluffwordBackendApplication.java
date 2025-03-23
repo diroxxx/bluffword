0a8cc6de033c926cbb8335fb87752a3f62bf4a87
diroxxx <che.dom29@gmail.com>
2025-03-23 06:52:23 +0100

    Initial backend commit

---
+++ backend/bluffword-backend/src/main/java/org/bluffwordbackend/BluffwordBackendApplication.java
@@ -0,0 +1,13 @@
+package org.bluffwordbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluffwordBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluffwordBackendApplication.class, args);
+    }
+
+}
---
+++ backend/bluffword-backend/src/test/java/org/bluffwordbackend/BluffwordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bluffwordbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluffwordBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
